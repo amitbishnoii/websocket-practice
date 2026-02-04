7ebc3d5f535b62001f47fe5dbfccd0df9c308a10
amitbishnoii <amitbishnoi29012@gmail.com>
2026-02-04 20:54:23 +0530

    practice socket.io events

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import socket from '../config/socket.js';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [message, setMessage] = useState("");
+
+  const handleSend = () => {
+    console.log('message sent');
+    socket.emit("message", message);
+    setMessage("");
+  };
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log('socket connected!', socket.id);
+    })
+
+    socket.on("recieve-message", (info) => {
+      console.log('got a message: ', info);
+    });
+
+    return () => {
+      socket.disconnect();
+    }
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" value={message} onChange={handleChange} placeholder="Message..."/>
+      <button onClick={handleSend}>Send</button>
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,43 @@
-import express from "express"
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from 'cors';
 
-const app = express()
+const port = 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server,
+    {
+        cors: {
+            origin: "http://localhost:5173",
+            methods: ["GET", "POST"],
+            credentials: true,
+        }
+    });
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+}))
 
 app.get("/", (req, res) => {
-    res.send("hello world")
-})
+    res.send("hello world");
+});
 
-app.listen(3000, () => {
-    console.log('app listening on port 3000');
-})
+io.on("connection", (socket) => {
+    console.log('user connected!', socket.id);
+
+    socket.on("message", (data) => {
+        console.log(data);
+        io.emit("recieve-message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`user ${socket.id} disconnected`);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+});
